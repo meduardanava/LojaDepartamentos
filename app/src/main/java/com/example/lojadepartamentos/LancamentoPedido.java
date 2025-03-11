5eb5a3ee79dc062d29a4bd9259c2808bbbf097db
Maria Eduarda Nava <meduardanava@gmail.com>
2025-03-10 21:40:54 -0300

    correção de erros

--- app/src/main/java/com/example/lojadepartamentos/LancamentoPedido.java
+++ app/src/main/java/com/example/lojadepartamentos/LancamentoPedido.java
@@ -1,23 +1,17 @@
 package com.example.lojadepartamentos;
 
-import androidx.annotation.ArrayRes;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -35,14 +29,13 @@ public class LancamentoPedido extends AppCompatActivity {
     private RadioButton rbAprazo;
     private ArrayList<Cliente> clientes;
     private ArrayList<Item> itens;
-    private int posicaoItem;
-    private int posicaoCliente;
+    private int posicaoItem = -1;
+    private int posicaoCliente = -1;
     private int numPedido;
-    private String[]vetAvista = new String[];
-    private String[]vetAprazo = new String[]{"Selecione a quantidade de parcelas", "1 vez",
+    private String[] vetAvista = {"À vista"};
+    private String[] vetAprazo = {"Selecione a quantidade de parcelas", "1 vez",
             "2 vezes", "3 vezes", "4 vezes", "5 vezes", "6 vezes"};
-    private String[]vetPedido = new String[];
-
+    private String[] vetPedido = {};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,19 +55,17 @@ public class LancamentoPedido extends AppCompatActivity {
         spListaCliente.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-
+                posicaoCliente = i;
+                tvErroCliente.setVisibility(View.GONE);
             }
 
             @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
+            public void onNothingSelected(AdapterView<?> adapterView) {}
         });
 
         spListaItem.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int posicao, long l) {
-
 
                 if (posicao > 0){
                     posicaoItem = posicao;
@@ -88,12 +79,8 @@ public class LancamentoPedido extends AppCompatActivity {
             }
         });
 
-        rgPagamento.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        rgPagamento.setOnCheckedChangeListener((group, checkedId) -> carregarPagamento());
 
-            }
-        });
         popularListaItem();
         popularListaCliente();
         atualizarListaPedido();
@@ -102,15 +89,15 @@ public class LancamentoPedido extends AppCompatActivity {
     private void popularListaCliente(){
 
         clientes = Controller.getInstancia().retornarCliente();
-        String[]vetorClientes = new String[clientes.size()+1];
+        String[] vetorClientes = new String[clientes.size() + 1];
         vetorClientes[0] = "Selecione o cliente";
 
         for (int i = 0; i < clientes.size(); i++){
             Cliente cliente = clientes.get(i);
-            vetorClientes[i+1] = cliente.getNome();
+            vetorClientes[i + 1] = cliente.getNome();
         }
 
-        ArrayAdapter adapterC = new ArrayAdapter(LancamentoPedido.this,
+        ArrayAdapter<String> adapterC = new ArrayAdapter(this,
                 android.R.layout.simple_dropdown_item_1line, vetorClientes);
 
         spListaCliente.setAdapter(adapterC);
@@ -119,15 +106,15 @@ public class LancamentoPedido extends AppCompatActivity {
    private void popularListaItem(){
 
         itens = Controller.getInstancia().retornarItem();
-        String[]vetorItens = new String[itens.size()+1];
+        String[] vetorItens = new String[itens.size() + 1];
         vetorItens[0] = "Selecione o item";
 
         for (int i = 0; i < itens.size(); i++){
             Item item = itens.get(i);
-            vetorItens[i+1] = item.getCodigo() + " - " + item.getDescricao();
+            vetorItens[i + 1] = item.getCodigo() + " - " + item.getDescricao();
         }
 
-        ArrayAdapter adapterI = new ArrayAdapter(LancamentoPedido.this,
+        ArrayAdapter<String> adapterI = new ArrayAdapter(this,
                android.R.layout.simple_dropdown_item_1line, vetorItens);
 
         spListaItem.setAdapter(adapterI);
@@ -135,27 +122,31 @@ public class LancamentoPedido extends AppCompatActivity {
 
    private void carregarPedido(){
 
-        int quantidade;
-
-        if (posicaoCliente <=0){
+        if (posicaoCliente <= 0){
             tvErroCliente.setVisibility(View.VISIBLE);
             return;
         }
 
-        if (edQuantidade.getText().toString().isEmpty()){
+        int quantidade;
+        String quantidadeStr = edQuantidade.getText().toString();
+
+        if (quantidadeStr.isEmpty()){
             edQuantidade.setError("A quantidade deve ser informada!");
             edQuantidade.requestFocus();
             return;
-        }else{
-            quantidade = Integer.parseInt(edQuantidade.getText().toString());
+        } try {
+            quantidade = Integer.parseInt(quantidadeStr);
 
             if (quantidade <= 0 ){
                 edQuantidade.setError("Informe uma quantidade maior que zero!");
                 edQuantidade.requestFocus();
                 return;
             }
+        } catch (NumberFormatException e){
+            edQuantidade.setError("Número inválido!");
+            edQuantidade.requestFocus();
+            return;
         }
-
         if (posicaoItem <= 0){
             tvErroItem.setVisibility(View.VISIBLE);
             return;
@@ -168,66 +159,60 @@ public class LancamentoPedido extends AppCompatActivity {
         pedido.setQuantidade(quantidade);
 
         Controller.getInstancia().listaPedido(pedido);
-        Toast.makeText(LancamentoPedido.this,
-                "Item adicionado no pedido!", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Item adicionado no pedido!",
+                Toast.LENGTH_LONG).show();
    }
 
    private void atualizarListaPedido(){
 
-        String texto = " ";
+        StringBuilder texto = new StringBuilder();
 
        ArrayList<Pedido> listaP = Controller.getInstancia().retornarPedido();
 
        for (Pedido pedido : listaP){
-            Item itens = pedido.getItem();
-            texto += itens.getCodigo() + " \t| " + itens.getDescricao() + " \t|"
-                    + itens.getValorUnitario() + "\n------------------------------------" +
-                    "---------------------------------------------\n";
+            Item item = pedido.getItem();
+            texto.append(item.getCodigo()).append(" | ")
+                    .append(item.getDescricao()).append(" | ")
+                    .append(item.getValorUnitario())
+                    .append("\n--------------------------------------------\n");
        }
-       tvListaPedido.setText(texto);
+       tvListaPedido.setText(texto.toString());
    }
 
    private void mostrarPedido(){
 
+       ArrayList<Pedido> listaPedidos = Controller.getInstancia().retornarPedido();
+        String[] vetPedido = new String[listaPedidos.size()];
 
-        listaPedido = Controller.getInstancia().retornarPedido();
-        String[]vetPedido =new String[listaPedidos.size()];
-
-        for (int i = 0 ; i < listaPedido.size(); i++ ){
-            Pedido pedido = listaPedido.get(i);
-            vetPedido[i] = pedido.getItem();
+        for (int i = 0 ; i < listaPedidos.size(); i++ ){
+            Pedido pedido = listaPedidos.get(i);
+            vetPedido[i] = pedido.getItem().getDescricao();
         }
-        ArrayAdapter adapterLP = new ArrayAdapter(LancamentoPedido.this,
+
+        ArrayAdapter<String> adapterLP = new ArrayAdapter(this,
                 android.R.layout.simple_dropdown_item_1line,vetPedido);
    }
 
    private void carregarPagamento(){
 
-        ArrayAdapter adapterPag = null;
+        ArrayAdapter<String> adapterPag;
 
         if (rbAvista.isChecked()){
-            adapterPag = new ArrayAdapter<>(LancamentoPedido.this,
-                    android.R.layout.simple_dropdown_item_1line, );
-
-        }
-        if (rbAprazo.isChecked()){
-            adapterPag = new ArrayAdapter(LancamentoPedido.this,
+            adapterPag = new ArrayAdapter<>(this,
+                    android.R.layout.simple_dropdown_item_1line, vetAvista);
+        } else if (rbAprazo.isChecked()){
+            adapterPag = new ArrayAdapter<>(this,
                     android.R.layout.simple_dropdown_item_1line, vetAprazo);
+        } else {
+            return;
         }
-        spListaItem.setAdapter(adapterPag);
 
+        spListaItem.setAdapter(adapterPag);
    }
 
    private void gerarNumPedido(){
 
         Random random = new Random();
-
-        int limiteSuperior = 1000;
-        int limiteInferior = 1;
-
-        numPedido = random.nextInt(limiteSuperior - limiteInferior
-                + 1) + limiteInferior;
-
-        return;
+        numPedido = random.nextInt(1000) + 1;
    }
 }
--- app/src/main/java/com/example/lojadepartamentos/MainActivity.java
+++ app/src/main/java/com/example/lojadepartamentos/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
                 abrirActivity(LancamentoPedido.class);
             }
         });
+
     }
 
     private void abrirActivity(Class<?> activity){
